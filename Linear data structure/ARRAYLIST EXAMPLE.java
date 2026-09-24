682437d8670bdd109484e4bf90cc66fb72095d75
Tarun <tarunar006@gmail.com>
2026-09-24 23:43:30 +0530

    Add example of ArrayList usage in Java

---
+++ Linear data structure/ARRAYLIST EXAMPLE.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main{
+    public static void main(String[] args){
+       ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+    } 
+}
